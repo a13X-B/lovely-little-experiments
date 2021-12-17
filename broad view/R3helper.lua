9ee967c34ac1cea90665127e283f6fd3f48c625e
a13X-B <53251919+a13X-B@users.noreply.github.com>
2021-12-17 23:01:34 +0500

    no shader version, fixed winding, love's default projection inverse

---
+++ broad view/R3helper.lua
@@ -0,0 +1,66 @@
+local m = love.math
+local g = love.graphics
+
+local projection = m.newTransform()
+local inverse = m.newTransform()
+local last_one
+local tmp = m.newTransform()
+
+local R3 = {}
+
+function R3.newProjection(perspective, width, height, hfov)
+	local p = perspective and 1 or 0
+	projection:setMatrix(
+		1, 0, 0, 0,
+		0, width/height, 0, 0,
+		0, 0, 1, 0,
+		0, 0, p, 1
+	)
+	inverse:setMatrix(
+		2/width,0,0,-1,
+		0,-2/height,0,1,
+		0,0,-1/10,0,
+		0,0,0,1
+	)
+	last_one = inverse:inverse():apply(projection)
+	return last_one
+end
+
+function R3.origin(projection)
+	g.replaceTransform(projection or last_one)
+end
+
+function R3.translate(x,y,z)
+	tmp:setMatrix(
+		1,0,0,x,
+		0,1,0,y,
+		0,0,1,z,
+		0,0,0,1
+	)
+	g.applyTransform(tmp)
+	tmp:reset()
+end
+
+function R3.rotate(i,j,k,w)
+	tmp:setMatrix(
+		1-2*j*j-2*k*k, 2*i*j+2*w*k, 2*i*k-2*w*j, 0,
+		2*i*j-2*w*k, 1-2*i*i-2*k*k, 2*j*k+2*w*i, 0,
+		2*i*k+2*w*j, 2*j*k-2*w*i, 1-2*i*i-2*j*j, 0,
+		0, 0, 0, 1
+	)
+	g.applyTransform(tmp)
+	tmp:reset()
+end
+
+function R3.scale(x,y,z)
+	tmp:setMatrix(
+		x,0,0,0,
+		0,y,0,0,
+		0,0,z,0,
+		0,0,0,1
+	)
+	g.applyTransform(tmp)
+	tmp:reset()
+end
+
+return R3
--- broad view/main.lua
+++ broad view/main.lua
@@ -1,22 +1,18 @@
 --[[
 TODO:
-- fix winding
 - fov
 - figure out z projection
 - textures
 - add camera
-- make a module
 ]]
 
 local g = love.graphics
 
-local w, h = g.getDimensions()
-local ar = w/h -- aspect ratio
+local R3 = require("R3helper")
 
-g.setMeshCullMode("none") -- no cull yet, winding is busted
+g.setMeshCullMode("back")
+g.setFrontFaceWinding("ccw")
 g.setDepthMode("less", true)
-
-local shader = g.newShader("shader.glsl")
 
 --cube mesh
 local vcf = {{"VertexPosition", "float", 3}}
@@ -31,12 +27,12 @@ local vt = {
 }
 
 local vm = {
-	1,2,3, 4,3,2,
-	5,7,6, 6,7,8,
-	3,8,7, 3,4,8,
-	1,5,6, 1,6,2,
-	3,7,5, 1,3,5,
-	4,6,8, 4,2,6,
+	1,3,2, 2,3,4,
+	5,6,7, 6,8,7,
+	3,7,8, 3,8,4,
+	1,6,5, 1,2,6,
+	3,5,7, 1,5,3,
+	4,8,6, 2,4,6,
 	}
 
 local cube = g.newMesh(vcf, vc, "triangles", "static")
@@ -45,61 +41,32 @@ cube:setVertexMap(vm)
 
 cube:attachAttribute("VertexColor", tex)
 
---simple projection matrix
-local projection = love.math.newTransform():setMatrix(
-	1/ar, 0, 0, 0,
-	0, 1, 0, 0,
-	0, 0, 1, 0,
-	0, 0, 1, 1
-)
-
-local scale = love.math.newTransform():setMatrix(
-	.3, 0, 0, 0,
-	0, .3, 0, 0,
-	0, 0, .3, 0,
-	0, 0, 0, 1
-)
-
-local translate = love.math.newTransform():setMatrix(
-	1, 0, 0, 0,
-	0, 1, 0, 0,
-	0, 0, 1, 2,
-	0, 0, 0, 1
-)
-
-local orient = love.math.newTransform()
+R3.newProjection(true, g.getDimensions())
 
 --takes axis angle representation
 local function set_orientation(x,y,z,a)
 	--let's turn axis angle to a quaternion, would've been great if we had one from the start
 	local l = math.sqrt(x*x+y*y+z*z)
-	assert(l ~= 0, "learn a thing or two about axis angle representation or quats")
 	x,y,z = x/l, y/l, z/l --normalize imaginary part
 	local w, c = math.sin(a/2), math.cos(a/2) --the real part is a sine of half an angle
 	--the imaginary part will get multiplied by cosine of half an angle
 	x,y,z = x*c, y*c, z*c -- so x, y, z, w is a quaternion now
-	orient:setMatrix( --that's a normalized quaternion to matrix conversion
-		1-2*y*y-2*z*z, 2*x*y+2*w*z, 2*x*z-2*w*y, 0,
-		2*x*y-2*w*z, 1-2*x*x-2*z*z, 2*y*z+2*w*x, 0,
-		2*x*z+2*w*y, 2*y*z-2*w*x, 1-2*x*x-2*y*y, 0,
-		0, 0, 0, 1
-	)
-	g.applyTransform(orient)
+	R3.rotate(x,y,z,w)
 end
 
-local final_transform = love.math.newTransform()
-function love.draw()
-	final_transform:reset()
-	final_transform:apply(projection)
-	final_transform:apply(translate)
-	--final_transform:apply(scale)
-	final_transform:apply(orient)
-	shader:send("transform", final_transform)
-	g.setShader(shader)
-	local t = love.timer.getTime()
-	set_orientation( math.cos(t*.1), math.sin(t*.1), 0, t % math.pi*2)--axis angle
+local c = g.newCanvas(g.getDimensions())
 
+function love.draw()
+	local t = love.timer.getTime()
+	g.draw(c)
+	--g.setCanvas({c, depth=true})
+	g.clear()
+	R3.origin()
+	R3.scale(.1,.1,.1)
+	--R3.translate(1,math.sin(t),1)
+	set_orientation( 0, 1, 0, t % math.pi*2)--axis angle
 	g.draw(cube)
+	g.setCanvas()
 end
 
 function love.keypressed(k,s,r)
